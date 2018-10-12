a8bcdb9029111125662250932a59dc3b579a00ab
AHDiniz <AHDiniz@github.com>
2018-10-12 18:58:07 -0300

    Starting to make the priority queue

--- include/priority_queue.h
+++ include/priority_queue.h
@@ -12,8 +12,13 @@
 
 #define PRIORITY_QUEUE_H_
 
+#include <stdbool.h>
+
 // Definition of the priority queue data type:
-typedef struct priorityQueue PriorityQueue;
+typedef struct pQueue PQueue;
+
+// Definition of the item comparision callback:
+typedef (int) (*PQItem_Compare) (void *, void *);
 
 /**
  * Function that creates a priority queue:
@@ -23,7 +28,7 @@ typedef struct priorityQueue PriorityQueue;
  * Condition: size that is appropriate to the computer's memory
  * Side effects: the priority queue is DYNAMICALLY ALLOCATED, REMEMBER TO DESTROY IT WHEN IT'S NOT BEING USED ANYMORE!!!
  */
-PriorityQueue *PQueue_Create(int size);
+PQueue *PQueue_Create(int size);
 
 /**
  * Function that destroys a priority queue:
@@ -33,8 +38,66 @@ PriorityQueue *PQueue_Create(int size);
  * Conditions: existent and allocated priority queue
  * Side effects: the pointer will point to NULL
  */
-PriorityQueue *PQueue_Destroy(ProrityQueue *priorityQueue);
+PQueue *PQueue_Destroy(PQueue *pQueue);
 
-/** */
+/**
+ * Function that checks if the queue is empty
+ * 
+ * Input: pointer to the queue
+ * Output: boolean that tells if the queue is or isn't empty
+ * Conditions: existent and allocated queue
+ * Side effects: none
+ */
+bool PQueue_Empty(PQueue *pQueue);
+
+/**
+ * Function that returns the size of the given queue
+ * 
+ * Input: pointer to the queue
+ * Output: amount of items in the queue
+ * Conditions: existent and allocated queue
+ * Side effects: none
+ */
+int PQueue_GetSize(PQueue *pQueue);
+
+/**
+ * Function that inserts a item with certain priority in the queue:
+ * 
+ * Inputs: pointer to the priority queue, pointer to the item, the priority that the item will have
+ * Output: an boolean that tells if the operation was or wasn't a success
+ * Conditions: the queue and the item must exist and be allocated and the key must be a valid one (depends on the context)
+ * Side effects: a new item is added to the queue
+ */
+bool PQueue_Insert(PQueue *pQueue, void *item, int priority);
+
+/**
+ * Function that returns the item with the most important priority in the queue
+ * 
+ * Input: pointer to the queue
+ * Output: pointer to the most important item
+ * Conditions: the queue must be existent and allocated
+ * Side effects: none
+ */
+void *PQueue_GetFirst(PQueue *pQueue);
+
+/**
+ * Function that removes the most important item from the queue
+ * 
+ * Input: pointer to the queue
+ * Ouput: pointer to the most important item
+ * Conditions: the queue must be existent and allocated
+ * Side effects: the item is removed from the queue
+ */
+void *PQueue_RemoveFirst(PQueue *pQueue);
+
+/**
+ * Function that changes the key from a given item:
+ * 
+ * Inputs: pointers to the queue, the item and the item comparision function and the item's new key
+ * Output: an integer that tells if the operation was or wasn't a success
+ * Conditions: existent and allocated queue and item, existent function and valid key (the last depends on the context)
+ * Side effects: the target item's key is changed to the entry key
+ */
+int PQueue_ChangeKey(PQueue *pQueue, void *item, int key, PQItem_Compare Cmp);
 
 #endif // PRIORITY_QUEUE_H_
---
+++ src/prority_queue.c
@@ -0,0 +1,8 @@
+#include "../include/priority_queue.h"
+
+struct pQueue
+{
+    void **queue; // The array of pointers to items that represents the queue (position 0 is for the size)
+};
+
+
