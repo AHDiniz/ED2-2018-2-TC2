1d2cec8747966e826361b81002977f15e88ba59e
AHDiniz <AHDiniz@github.com>
2018-10-24 21:54:32 -0300

    Reimplementing worst fit
    Don't worry, it's wrong...

--- src/fit.c
+++ src/fit.c
@@ -18,7 +18,30 @@
 // Implementing the worst fit heuristic:
 int WorstFit(int *weights, int length, int limit)
 {
-    return 0;
+    PQueue *bins = PQueue_Create(length); // Creating a new priority queue to hold the bins
+    
+    for (int i = 0; i < length; i++) // Looping for every weight given
+    {
+        if (PQueue_Empty(bins)) // Checking if the queue is empty
+            PQueue_Insert(bins, weights[i]);
+        else
+        {
+            // Getting the bin with the biggest empty space:
+            int first = PQueue_GetFirst(bins);
+            
+            // Checking if the weight fits in the bin:
+            if (first + weights[i] <= limit)
+            {
+                first = PQueue_RemoveFirst(bins); // Removing the bin with the biggest empty space from the queue
+                PQueue_Insert(bins, weights[i] + first);
+            }
+            else PQueue_Insert(bins, weights[i]); // If the weight doesn't fit, create new bin
+        }
+    }
+
+    int result = PQueue_GetSize(bins);
+    PQueue_Destroy(bins);
+    return result;
 }
 
 /**
